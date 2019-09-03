308eee4b19aa53510ebac5900855de04d63f67e5
HankinsPan <hankins.home@outlook.com>
2019-09-03 12:06:23 +0800

    add dispatch step1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,94 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+//
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const appState = {
+  title: {
+    text: 'React.js',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 内容',
+    color: 'blue'
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function stateChanger(state, action) {
+  switch (action.type) {
+    case 'UPDATE_TITLE_TEXT':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
+    case 'UPDATE_TITLE_COLOR':
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
+    default:
+      return state
+  }
+}
+
+function createStore(state, stateChanger) {
+  let listeners = []
+
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = stateChanger(state, action)
+    listeners.forEach((listener) => listener())
+  }
+  return { getState, dispatch, subscribe }
+}
+
+function renderApp(newAppState, oldAppState = {}) {
+  if (newAppState === oldAppState) return
+  console.log("==== render app .. ")
+  renderTitle(newAppState.title, oldAppState.title)
+  renderContent(newAppState.content, oldAppState.content)
+}
+
+function renderTitle(newTitle, oldTitle = {}) {
+  if (newTitle === oldTitle) return
+  console.log("==== render title .. ")
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = newTitle.text
+  titleDOM.style.color = newTitle.color
+}
+
+function renderContent(newContent, oldContent = {}) {
+  if (newContent === oldContent) return
+  console.log("==== render content .. ")
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = newContent.text
+  contentDOM.style.color = newContent.color
+}
+
+const store = createStore(appState, stateChanger)
+let oldState = store.getState()
+store.subscribe(() => {
+  const newStote = store.getState()
+  renderApp(newStote, oldState)
+  oldState = newStote
+})
+
+
+renderApp(appState)
+store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: 'React.js 小书 是一本好书' })
+store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: '#37c' })
